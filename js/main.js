c3b3e20422777e10945b84d85660fb92956ce578
alex1gtp <alex1gtp@gmail.com>
2017-03-23 09:24:59 +0200

    navbar-remake

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-;(function () {
+$(function() {
+    $('body').addClass('js');
+
+    var $hamburger = $('.hamburger'),
+        $nav = $('#site-nav'),
+        $masthead = $('#masthead');
+
+    $hamburger.click(function() {
+      $(this).toggleClass('is-active');
+      $nav.toggleClass('is-active');
+      $masthead.toggleClass('is-active');
+      return false;
+    })
+});
+(function () {
 
 	'use strict';
 
